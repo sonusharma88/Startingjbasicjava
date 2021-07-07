5dd06b1ab91850a41f64cf716a4dd89447d3f49d
Sonu Sharma <sonusharmacse88@gmail.com>
2021-07-07 19:03:15 +0530

    add to loops while and do while aand for loop

--- src/Loops.java
+++ src/Loops.java
@@ -1,2 +1,45 @@
 public class Loops {
+    // loops is used to  same contant Repeated
+    // so used to for loops
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            System.out.println("Kashish  "    +i);
+        }
+
+
+    for(int k=7; k>=0;k--) {
+        System.out.println(1 * k);
+    }
+    for(int s=1; s<=10;s++) {
+        System.out.println(2 * s);
+    }
+
+    // while loop
+
+        int y=1;
+    while(y<=10) {
+        System.out.println(7*y);
+           y++;
+    }
+    int i =4;
+    while(i>=0){
+        System.out.println(1*i);
+        i--;
+    }
+       int q=0;
+    while(q<5){
+        System.out.println("MONIKA......");
+        q++;
+        }
+    // do while loop
+        int r=0;
+     do{
+         System.out.println("seema..."  +r);
+         r++;
+     }
+
+         while(r<5);
+
+
+    }
 }
