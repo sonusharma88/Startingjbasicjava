8fd90549833c7154507037178426cdc6d1c8ad99
Sonu Sharma <sonusharmacse88@gmail.com>
2021-07-07 10:11:14 +0530

    This project Starting core java and name and variables

---
+++ src/Nameprint.java
@@ -0,0 +1,7 @@
+public class Nameprint {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("RAJESH");
+        System.out.println("Ram");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,35 @@
+public class Variables {
+    public static void main(String[] args){
+        // String is used to only names
+        String name = "Love";
+        System.out.println(name);
+
+        String cityname = "Noida";
+        System.out.println(cityname);
+
+        String villagename = "Pinahat";
+        System.out.println(villagename);
+
+        //int is used to numeric values
+        int price = 22;
+        System.out.println(price);
+
+        int bestprice = 65;
+        System.out.println(bestprice);
+
+        int homeno = 5645;
+        System.out.println(homeno);
+
+        // double is used to decimal no
+        // double is used for  point no
+        double marks = 87.44;
+        System.out.println(marks);
+
+        double percentage = 565.67;
+        System.out.println(percentage);
+
+
+
+
+    }
+}
