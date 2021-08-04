40a59fda7a4af251348eb2789ad8895a20bd5ff1
Sonu Sharma <sonusharmacse88@gmail.com>
2021-08-04 11:32:53 +0530

    add to code Abstract Method Demo

---
+++ src/inheritance/AbstractMethodDemo.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public abstract class AbstractMethodDemo {
+
+
+    // a method without method body is called abstract method
+
+    public abstract void demo();
+
+
+    //  at least one abstract method that is called abstract class
+    public void m1(){
+        System.out.println("This is a normal method ..");
+    }
+
+}
+
+
+
